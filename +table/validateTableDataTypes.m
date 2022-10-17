31548c14606035749194b9e7b590985809da3bef
David Clemens <daclem@gmx.de>
2022-10-17 16:39:52 +0200

    Add 2 table validation functions

---
+++ +table/validateTableDataTypes.m
@@ -0,0 +1,68 @@
+function tf = validateTableDataTypes(T,expectedFormatSpec)
+% validateTableDataTypes  Test if table has expected data types
+%   VALIDATETABLEDATATYPES tests if table T contains all of the expected
+%     data types provided as format specifiers in expectedFormatSpec.
+%
+%   Syntax
+%     tf = VALIDATETABLEDATATYPES(T,expectedFormatSpec)
+%
+%   Description
+%     tf = VALIDATETABLEDATATYPES(T,expectedFormatSpec)  Test if all columns in
+%       table T are of a specific data type specified as format specifier in 
+%       expectedFormatSpec. This comparison is positional.
+%
+%   Example(s)
+%     tf = VALIDATETABLEDATATYPES(table(1,2,categorical(3),'VariableNames',{'a','b','c'}),{'%f','%n','%C'})
+%       returns tf = true.
+%     tf = VALIDATETABLEDATATYPES(table(1,2,3,'VariableNames',{'a','b','c'}),{'%f','%n','%C'})
+%       returns tf = false.
+%
+%   Input Arguments
+%     T - Input table
+%       table
+%         The table against which the expected format specifiers are tested.
+%
+%     expectedFormatSpec - expected column formatSpec
+%       cellstr vector
+%         Expected column data type specified as cellstr of format specifiers.
+%         Is required to have a valid entry for each column in T. The comparison
+%         is positional, meaning that the first element in expectedFormatSpec
+%         tests the data type of the first column in T, etc.
+%         For details on the formatSpec class see run 'help table.formatSpec'
+%
+%
+%   Output Arguments
+%     tf - test result
+%       logical scalar
+%         True if all columns of T match the data types specified as format
+%         specifiers in expectedFormatSpec.
+%
+%
+%   Name-Value Pair Arguments
+%
+%
+%   See also TABLE.FORMATSPEC, TABLE.VALIDATETABLEVARIABLENAMES
+%
+%   Copyright (c) 2022-2022 David Clemens (dclemens@geomar.de)
+%
+
+    import table.formatSpec
+    
+    % Validate inputs
+    validateattributes(T,{'table'},{},mfilename,'T',1)
+    nCols = size(T,2);
+    validateattributes(expectedFormatSpec,{'cell'},{'vector','numel',nCols},mfilename,'expectedFormatSpec',2)
+    
+    assert(iscellstr(expectedFormatSpec),...
+        'Utilities:table:validateTableDataTypes:invalidType',...
+        'Expected input number 2, expectedFormatSpec, to be one of these types:\n\ncellstr.\n\nInstead its type was %s.',class(expectedFormatSpec))
+
+    fSpecMembers = enumeration('table.formatSpec');
+    expectedFormatSpec = cellfun(@(s) validatestring(s,cat(1,fSpecMembers.FormatSpec),mfilename,'expectedFormatSpec',2),expectedFormatSpec,'un',0);
+    
+    % Get the formatSpec for each column
+    fSpec   = arrayfun(@(col) formatSpec(class(T{1,col})),1:nCols);
+    
+    % Compare to expectations
+    tf = all(arrayfun(@(fs,efs) any(ismember(fs.FormatSpec,efs)),fSpec,expectedFormatSpec));
+end
---
+++ +table/validateTableVariableNames.m
@@ -0,0 +1,79 @@
+function [tf,varargout] = validateTableVariableNames(T,expectedVariableNames)
+% validateTableVariableNames  Test if table has expected column names
+%   VALIDATETABLEVARIABLENAMES tests if table T contains all of the expected
+%     column names (variable names) provided in expectedVariableNames.
+%
+%   Syntax
+%     tf = VALIDATETABLEVARIABLENAMES(T,expectedVariableNames)
+%     [tf,ind] = VALIDATETABLEVARIABLENAMES(__)
+%
+%   Description
+%     tf = VALIDATETABLEVARIABLENAMES(T,expectedVariableNames)  Test if table 
+%       T has all variable names defined in expectedVariableNames.
+%     [tf,ind] = VALIDATETABLEVARIABLENAMES(__)  Also return the column index at
+%       which the expectedVariableNames are found in T.
+%
+%   Example(s)
+%     tf = VALIDATETABLEVARIABLENAMES(table(1,2,3,'VariableNames',{'a','b','c'}),{'a','c','b','d'})
+%       returns tf = false.
+%     tf = VALIDATETABLEVARIABLENAMES(table(1,2,3,'VariableNames',{'a','b','c'}),{'a','c'})
+%       returns tf = true.
+%     [tf,ind] =VALIDATETABLEVARIABLENAMES(table(1,2,3,'VariableNames',{'a','b','c'}),{'a','c'})
+%       returns tf = true and ind = [1,3].
+%
+%
+%   Input Arguments
+%     T - Input table
+%       table
+%         The table against which the expected variable names are tested.
+%
+%     expectedVariableNames - expected column names
+%       cellstr vector
+%         Cellstr list of variable names that are required to be a member of
+%         table T.
+%
+%
+%   Output Arguments
+%     tf - test result
+%       logical scalar
+%         True if all entries in expectedVariableNames are variable names in
+%         table T.
+%     ind - column indices
+%       integer vector
+%         The column indices at which the expectedVariableNames are found in
+%         table T returned as an integer vector.
+%
+%
+%   Name-Value Pair Arguments
+%
+%
+%   See also TABLE.VALIDATETABLEDATATYPES
+%
+%   Copyright (c) 2022-2022 David Clemens (dclemens@geomar.de)
+%
+    
+    nargoutchk(0,2)
+    
+    % Validate inputs
+    validateattributes(T,{'table'},{},mfilename,'T',1)
+    validateattributes(expectedVariableNames,{'cell'},{'vector'},mfilename,'expectedVariableNames',2)
+    
+    assert(iscellstr(expectedVariableNames),...
+        'Utilities:table:validateTableVariableNames:invalidType',...
+        'Expected input number 2, expectedVariableNames, to be one of these types:\n\ncellstr.\n\nInstead its type was %s.',class(expectedVariableNames))
+
+    nCols = size(T,2);
+    nExpectedVariableNames = numel(expectedVariableNames);
+    assert(nExpectedVariableNames >= nCols,...
+        'Utilities:table:validateTableVariableNames:notEnoughVariableNames',...
+        'There were only %u espected variable names provided, even though the table has %u columns.',nExpectedVariableNames,nCols)
+    
+    % Test if all variable names occur in table and their column index
+    [im,imInd] = ismember(expectedVariableNames,T.Properties.VariableNames);
+    tf = all(im);
+    
+    % Optionally return column indices
+    if nargout == 2
+        varargout{1} = imInd;
+    end
+end
