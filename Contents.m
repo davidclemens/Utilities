4a004e921fbc88900478f381c5a90aeafe44bb9c
David Clemens <daclem@gmx.de>
2022-11-14 11:39:34 +0100

    Fix Contents.m

--- Contents.m
+++ Contents.m
@@ -1,14 +1,16 @@
 % Utilities
-% Version 1.3.0 (2022.10.0) 09-Oct-2022
+% Version 1.5.1 (2022.11.1) 14-Nov-2022
 %
 % Utilities
 %   table - Table I/O
 %   Tests - Unit tests
 %   toolbox - Query toolbox/package metadata
+%   UI - UI related utilities
 %
 % Files
 %   arrayhom - Homogenizes arrays with varying shapes
 %   dec2basen - Decimal integer to its base n representation using custom symbols
 %   findContinuousSections - Find continuous sequences of values above a threshold
 %   readApolloFile - Reads the data file from an Apollo AS-C6L DIC analyzer
+%   readCalkulateResultFile - Reads a result file from AlkalinityAnalysis
 %   sub2excelRange - Convert two subscript pairs to Excel range 
